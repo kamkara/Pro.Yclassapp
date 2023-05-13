c6c0876c5a530d0bc12ac15a3ef899ba8909d1da
kamkara <lnclass.kamkara@gmail.com>
2023-05-13 00:53:17 +0000

    Add Membership

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  
+  ################## USERS  ##########################
+  devise_scope :user do
+    get 'profil', to: 'devise/registrations#edit'
+    get 'student-sign-in', to: 'devise/sessions#new'
+    get 'student-sign-up', to: 'devise/registrations#new', as: "new_user_registration"
+    delete 'deconnecter',  to: "devise/sessions#destroy", as: "destroy_user_session_path"
+  end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homepage#index"
 end
